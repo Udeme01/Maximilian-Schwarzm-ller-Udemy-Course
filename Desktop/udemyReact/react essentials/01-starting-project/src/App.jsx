4e486b2864e1fa6d80fb57ea7ff67bffbf022600
Udeme01 <emmanueluddy2@gmail.com>
2024-01-20 18:34:31 +0100

    added feature 'TabButon'

--- Desktop/udemyReact/react essentials/01-starting-project/src/App.jsx
+++ Desktop/udemyReact/react essentials/01-starting-project/src/App.jsx
@@ -1,9 +1,18 @@
-import { Component } from "react";
+// import { Component } from "react";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
+import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selected) {
+    setSelectedTopic(selected);
+  }
+
   return (
     <div>
       <Header />
@@ -20,6 +29,29 @@ function App() {
             <CoreConcepts {...CORE_CONCEPTS[2]} />
             <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+
+        {/* EXAMPLES */}
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {!selectedTopic ? <p>Please select a topic</p> : null}
+          {selectedTopic ? (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          ) : null}
         </section>
       </main>
     </div>
